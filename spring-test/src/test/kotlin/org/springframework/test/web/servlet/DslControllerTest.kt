a748f7f4143fc1c3d3daea1954ffd7f898677487
Clint Checketts <clint.checketts@domo.com>
2019-02-22 14:14:59 -0700

    Add andExpectCustom examples

--- spring-test/src/test/kotlin/org/springframework/test/web/servlet/DslControllerTest.kt
+++ spring-test/src/test/kotlin/org/springframework/test/web/servlet/DslControllerTest.kt
@@ -1,6 +1,5 @@
 package org.springframework.test.web.servlet
 
-import junit.framework.TestCase.assertTrue
 import org.hamcrest.CoreMatchers
 import org.junit.Assert.assertEquals
 import org.junit.jupiter.api.Test
@@ -10,15 +9,10 @@ import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.util.AssertionErrors
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.MvcResult
-import org.springframework.test.web.servlet.ResultMatcher
 import org.springframework.test.web.servlet.result.JsonPathResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.result.ModelResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 import javax.servlet.http.Cookie
@@ -61,7 +55,7 @@ class DslControllerTest {
 			content { json("""{"surname":"Petr"}""", false) }  //JsonAssert support (non-strict is the default)
 			"$.surname" jsonPathIs name //JsonPath
 
-			"surname" jsonPath { value("Petr")}
+			"surname" jsonPath { value("Petr") }
 			jsonPath("surname") { value("Petr") }
 
 			match(HandlerMethod("helloJson"))
@@ -70,31 +64,33 @@ class DslControllerTest {
 
 	@Test
 	fun helloGet() {
-		mockMvc.get("/hello?name=Petr") andDo {
-			handle(MockMvcResultHandlers.print())
-		} andExpect {
+		mockMvc.get("/hello?name=Petr")
+				.andDo {
+					handle(MockMvcResultHandlers.print())
+				}.andExpect {
 
-			match(status().isOk)
+					match(status().isOk)
 
-			content { contentTypeCompatibleWith(MediaType.TEXT_HTML) }
-			view { name("hello") }
+					content { contentTypeCompatibleWith(MediaType.TEXT_HTML) }
+					view { name("hello") }
 
-			model {
-				size<Any>(1)
-				attribute("name", "Petr")
-			}
+					model {
+						size<Any>(1)
+						attribute("name", "Petr")
+					}
 
-			xpath("//h1") {
-				nodeCount(1)
-			}
+					xpath("//h1") {
+						nodeCount(1)
+					}
 
-			xpath("""//span[@class="name"]""") {
-				nodeCount(1)
-				string("Petr")
-			}
-		} andHandle {
-			response.contentAsString.contains("Hello world")
-		}
+					xpath("""//span[@class="name"]""") {
+						nodeCount(1)
+						string("Petr")
+					}
+				}.andHandle {
+					response.contentAsString.contains("Hello world")
+				}
+
 	}
 
 	@Test
@@ -127,7 +123,7 @@ class DslControllerTest {
 	fun `test post page`() = mockMvc.post("/hello") {
 		contentType = MediaType.APPLICATION_FORM_URLENCODED
 		param("surname", "Balat")
-	} andExpect {
+	}.andExpect {
 		xpath("//h1") {
 			nodeCount(1)
 		}
@@ -136,12 +132,10 @@ class DslControllerTest {
 			string("Balat")
 		}
 
-//		expectCustom(::ClintMatchers)
-//		{
-//			model<HelloPostDto>("helloPostDto") {
-//				assertEquals("Balat", surname)
-//			}
-//		}
+	}.andExpectCustom(::ClintMatchers) {
+		model<HelloPostDto>("helloPostDto") {
+			assertEquals("Balat", surname)
+		}
 	}
 
 
@@ -151,14 +145,15 @@ class DslControllerTest {
 			contentType = MediaType.APPLICATION_JSON
 			content = """{"surname": "Jack"}"""
 			cookie(Cookie("cookieName", "Extra Things"))
-		} andDo {
+		}.andDo {
 			print()
-		} andExpect {
+		}.andExpect {
 			status { isBadRequest }
 			content { json("""{"surname":"Jack"}""") }
 			content { json("""{"surname":"Jack", "extraName":"Extra Things"}""", true) }
-		}
-//				expectCustom({ ClintMatchers(it) }) { "$.surname" jsonPathIs "Jack" } //builder,actions, and expects can be called multiple times
+		}.andExpectCustom(::ClintMatchers) {
+			"$.surname" jsonPathIs "Jack"
+		} //builder,actions, and expects can be called multiple times
 	}
 
 	@Test
