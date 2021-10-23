2804b2f8ee38c46aac0ed6ece433331793fa5b18
Enric Sala <enric.sala@teampicnic.com>
2021-10-23 15:18:17 +0200

    Option 2: drop the unwrapping, propagate `RuntimeException`

--- spring-tx/src/test/kotlin/org/springframework/transaction/interceptor/AbstractCoroutinesTransactionAspectTests.kt
+++ spring-tx/src/test/kotlin/org/springframework/transaction/interceptor/AbstractCoroutinesTransactionAspectTests.kt
@@ -218,7 +218,11 @@ abstract class AbstractCoroutinesTransactionAspectTests {
 			}
 			catch (actual: Exception) {
 				if (rollbackException) {
-					assertThat(actual).hasMessage(tex.message).isInstanceOf(tex::class.java)
+					Assertions.assertThat(actual)
+						.isExactlyInstanceOf(RuntimeException::class.java)
+						.cause
+						.isExactlyInstanceOf(RuntimeException::class.java)
+						.hasCause(tex)
 				} else {
 					assertThat(actual).hasMessage(ex.message).isInstanceOf(ex::class.java)
 				}
@@ -291,7 +295,11 @@ abstract class AbstractCoroutinesTransactionAspectTests {
 				itb.setName(name)
 			}
 			catch (actual: Exception) {
-				Assertions.assertThat(actual).isEqualTo(ex)
+				Assertions.assertThat(actual)
+					.isExactlyInstanceOf(RuntimeException::class.java)
+					.cause
+					.isExactlyInstanceOf(RuntimeException::class.java)
+					.hasCause(ex)
 			}
 			// Should have invoked target and changed name
 			assertThat(itb.getName()).isEqualTo(name)
