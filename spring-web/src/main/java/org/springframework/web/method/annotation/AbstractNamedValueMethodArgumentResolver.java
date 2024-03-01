ac757f43e632a2c119acbbd3f3812ea84d72bbae
Sébastien Deleuze <sebastien.deleuze@broadcom.com>
2024-03-01 14:44:37 +0100

    WIP

--- spring-web/src/main/java/org/springframework/web/method/annotation/AbstractNamedValueMethodArgumentResolver.java
+++ spring-web/src/main/java/org/springframework/web/method/annotation/AbstractNamedValueMethodArgumentResolver.java
@@ -26,6 +26,7 @@ import kotlin.reflect.KFunction;
 import kotlin.reflect.KParameter;
 import kotlin.reflect.jvm.ReflectJvmMapping;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.ConversionNotSupportedException;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.config.BeanExpressionContext;
@@ -282,7 +283,11 @@ public abstract class AbstractNamedValueMethodArgumentResolver implements Handle
 
 		WebDataBinder binder = binderFactory.createBinder(webRequest, null, namedValueInfo.name);
 		try {
-			arg = binder.convertIfNecessary(arg, parameter.getParameterType(), parameter);
+			Class<?> parameterType = parameter.getParameterType();
+			if (KotlinDetector.isKotlinPresent() && KotlinDetector.isInlineClass(parameterType)) {
+				parameterType = BeanUtils.findPrimaryConstructor(parameterType).getParameterTypes()[0];
+			}
+			arg = binder.convertIfNecessary(arg, parameterType, parameter);
 		}
 		catch (ConversionNotSupportedException ex) {
 			throw new MethodArgumentConversionNotSupportedException(arg, ex.getRequiredType(),
